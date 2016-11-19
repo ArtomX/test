bd0700248c4da36f902436b015074cf4b9d0665c
Eremey <markovka.markovka_1@mail.ru>
2016-11-19 22:32:16 +0300

    Создал систему логирования

---
+++ NewTestVK_3/app/src/main/java/com/example/eremey/newtestvk/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.eremey.newtestvk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKList;
+import com.vk.sdk.util.VKUtil;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends Activity {
+    private  String [] scope=new String[]{VKScope.MESSAGES,VKScope.FRIENDS,VKScope.WALL};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VKSdk.login(this,scope);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                Toast.makeText(getApplicationContext(),"Good connection",Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(getApplicationContext()," Error",Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
