ee87ebce570d8785b26fa5eeae461e794d375e8a
Eremey <markovka.markovka_1@mail.ru>
2016-11-21 00:41:03 +0300

    Добавил меню в toolbar

--- NewTestVK_3/app/src/main/java/com/example/eremey/newtestvk/MainActivity.java
+++ NewTestVK_3/app/src/main/java/com/example/eremey/newtestvk/MainActivity.java
@@ -2,31 +2,26 @@ package com.example.eremey.newtestvk;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
 import android.widget.Toast;
 
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
-import com.vk.sdk.api.VKApi;
-import com.vk.sdk.api.VKApiConst;
+
 import com.vk.sdk.api.VKError;
-import com.vk.sdk.api.VKParameters;
-import com.vk.sdk.api.VKRequest;
-import com.vk.sdk.api.VKResponse;
-import com.vk.sdk.api.model.VKList;
-import com.vk.sdk.util.VKUtil;
-
-import java.util.Arrays;
 
 
-public class MainActivity extends Activity {
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
     private  String [] scope=new String[]{VKScope.MESSAGES,VKScope.FRIENDS,VKScope.WALL};
 
     @Override
@@ -34,7 +29,34 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         VKSdk.login(this,scope);
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+       // if (id == R.id.action_wall) {
+         //   Toast.makeText(MainActivity.this,getString(R.string.action_wall),Toast.LENGTH_LONG).show();
+        switch(id){
+            case R.id.action_wall:
+                Toast.makeText(MainActivity.this,getString(R.string.action_wall),Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_friends:
+                Toast.makeText(MainActivity.this,getString(R.string.action_friends),Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_messages:
+                Toast.makeText(MainActivity.this,getString(R.string.action_messages),Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -50,5 +72,7 @@ public class MainActivity extends Activity {
         })) {
             super.onActivityResult(requestCode, resultCode, data);
         }
+
+
     }
 }
