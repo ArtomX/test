f6d92078bdeaebbc4a2e9e6132a516fbfcc8ac56
Eremey <markovka.markovka_1@mail.ru>
2016-11-21 00:50:11 +0300

    Добавил menu в toolbar + привел код в порядок

--- NewTestVK_3/app/src/main/java/com/example/eremey/newtestvk/Application.java
+++ NewTestVK_3/app/src/main/java/com/example/eremey/newtestvk/Application.java
@@ -2,7 +2,6 @@ package com.example.eremey.newtestvk;
 
 
 import android.content.Intent;
-
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKSdk;
--- NewTestVK_3/app/src/main/java/com/example/eremey/newtestvk/MainActivity.java
+++ NewTestVK_3/app/src/main/java/com/example/eremey/newtestvk/MainActivity.java
@@ -1,78 +1,66 @@
 package com.example.eremey.newtestvk;
 
-import android.app.Activity;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.widget.Toast;
-
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
-
 import com.vk.sdk.api.VKError;
-
-
 import android.support.v7.app.AppCompatActivity;
 
-
-
 public class MainActivity extends AppCompatActivity {
-    private  String [] scope=new String[]{VKScope.MESSAGES,VKScope.FRIENDS,VKScope.WALL};
+    private String[] scope = new String[]{VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VKSdk.login(this,scope);
-
+        VKSdk.login(this, scope);
     }
-
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-       // if (id == R.id.action_wall) {
-         //   Toast.makeText(MainActivity.this,getString(R.string.action_wall),Toast.LENGTH_LONG).show();
-        switch(id){
+
+        switch (id) {
             case R.id.action_wall:
-                Toast.makeText(MainActivity.this,getString(R.string.action_wall),Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, getString(R.string.action_wall), Toast.LENGTH_LONG).show();
                 break;
             case R.id.action_friends:
-                Toast.makeText(MainActivity.this,getString(R.string.action_friends),Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, getString(R.string.action_friends), Toast.LENGTH_LONG).show();
                 break;
             case R.id.action_messages:
-                Toast.makeText(MainActivity.this,getString(R.string.action_messages),Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, getString(R.string.action_messages), Toast.LENGTH_LONG).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
             @Override
             public void onResult(VKAccessToken res) {
-                Toast.makeText(getApplicationContext(),"Good connection",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Good connection", Toast.LENGTH_LONG).show();
             }
+
             @Override
             public void onError(VKError error) {
-                Toast.makeText(getApplicationContext()," Error",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), " Error", Toast.LENGTH_LONG).show();
             }
         })) {
             super.onActivityResult(requestCode, resultCode, data);
         }
-
-
     }
+
 }
